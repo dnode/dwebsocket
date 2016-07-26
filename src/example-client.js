b81eae12f75b6ce0d2c3f05b7380ceb2f73951e1
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-07-26 12:54:57 +0200

    Formatting

--- src/example-client.js
+++ src/example-client.js
@@ -4,8 +4,8 @@ const dwebsocket = require('./lib.js').Client;
 
 const client = new dwebsocket.Client()
   .plugin(dwebsocket.authenticate('sharaal', 'sharaal'))
-  .plugin(require('./client/example-console.log.js'))
   .plugin(dwebsocket.reconnect())
+  .plugin(require('./client/example-console.log.js'))
   .connect(`ws://${process.env.HOST || '127.0.0.1'}:${process.env.PORT}`);
 
 client.emit('example', { example: 'client.emit' });
